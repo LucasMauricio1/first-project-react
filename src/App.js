f3d52ea9eaa76b881fa2b2fce12065b30dd4e219
LucassMCoreea <lucasmauriciofc@gmail.com>
2023-03-07 13:47:14 -0300

    my first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './style.css'
+
+const App = () => {
+  const hello = "Hello React";
+
+  return (
+    <div>
+      <h1>{hello}</h1>
+    </div>
+  );
+};
+
+export default App;
